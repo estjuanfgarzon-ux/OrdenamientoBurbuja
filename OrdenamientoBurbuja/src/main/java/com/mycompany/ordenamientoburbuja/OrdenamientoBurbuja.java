c8474ff64343ff3f123db08f98e18b43ae1c104c
Juan Felipe Garzon Varon <est.juanf.garzon@unimilitar.edu.co>
2026-04-09 10:46:24 -0500

    aca optimizamos el main mediante la implementacion de metodos

--- OrdenamientoBurbuja/src/main/java/com/mycompany/ordenamientoburbuja/OrdenamientoBurbuja.java
+++ OrdenamientoBurbuja/src/main/java/com/mycompany/ordenamientoburbuja/OrdenamientoBurbuja.java
@@ -4,23 +4,28 @@ import java.util.Arrays;
 public class OrdenamientoBurbuja {
 
     public static void main(String[] args) {
-        int[] arr = {45, 22, 11, -4, 7, 3};
-
+       
+        int []arr = {45, 22, 11, -4, 7, 3};
+       
+        System.out.println(Arrays.toString(arr));
+       
+        //Ordenar el arreglo
+        OrdenArreglo(arr);
+       
+        //arreglo ordenado
         System.out.println(Arrays.toString(arr));
 
-        int n = arr.length;
-
-        // Ordenamiento burbuja
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - 1 - i; j++) {
-                if (arr[j] > arr[j + 1]) {
+    }
+    public static void OrdenArreglo(int []arr){
+      int n =arr.length;
+        for (int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+                if(arr[i] > arr[j]){
                     int aux = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = aux;
+                    arr[j]=arr[i];
+                    arr[i]=aux;
                 }
             }
-        }
-
-        System.out.println(Arrays.toString(arr));
+        }  
     }
 }
